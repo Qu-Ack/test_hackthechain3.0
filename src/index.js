799e2286760b36ac873f3ac9825fb5844ff9e176
quack <quack@quack.quack>
2025-02-22 16:03:33 +0530

    dasdg

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("ok this is working hellodsafkhagshdl");
+  res.send("this is working hellodsafkhagshdl");
 });
 
 app.listen(3000, "0.0.0.0", () => {
