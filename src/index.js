cce38294f23683114beb7447b2648e3d59c1be43
quack <quack@quack.quack>
2025-02-22 13:53:15 +0530

    server test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("inside src folder");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
