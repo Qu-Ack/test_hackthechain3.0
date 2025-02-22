52ff67abfb42cc3009880c5e345bce6d2384cffb
quack <quack@quack.quack>
2025-02-22 23:58:12 +0530

    dakdakshg

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("ZORBA IS WINNING!");
+  res.send("ZORBA ISS WINNING!");
 });
 
 app.listen(3000, "0.0.0.0", () => {
