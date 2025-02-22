20fb8d8e9df8f69ceea7d86b4832384d7dadafd1
quack <quack@quack.quack>
2025-02-22 16:38:01 +0530

    zorba

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("dkashgskg is working hellodsafkhagshdl");
+  res.send("zorba is working");
 });
 
 app.listen(3000, "0.0.0.0", () => {
