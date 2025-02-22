25b0533329afb719a26f98f0c6e3af12d115ffd5
quack <quack@quack.quack>
2025-02-22 15:14:20 +0530

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("ok this is working ");
+  res.send("ok this is working hello");
 });
 
 app.listen(3000, "0.0.0.0", () => {
