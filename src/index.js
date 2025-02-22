13997171ff775e33a7b3bf974e3cb9a16c3e786d
quack <quack@quack.quack>
2025-02-22 23:26:46 +0530

    dakhsg

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("ZORBA is working!");
+  res.send("ZORBA2 is working!");
 });
 
 app.listen(3000, "0.0.0.0", () => {
