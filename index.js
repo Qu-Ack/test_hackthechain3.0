889d48ce97b5c8242acf3ba564c9b8c61827eee6
quack <quack@quack.quack>
2025-03-29 17:04:50 +0530

    025-test orchestration service

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello, world");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("ZORAB hackathon !");
+});
+
+app.listen(3000, "0.0.0.0", () => {
+	console.log("server started on port 3000");
+});
