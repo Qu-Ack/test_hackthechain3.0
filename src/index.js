64b879c8f52e18a0791094e5e8e29caacb860d63
quack <quack@quack.quack>
2025-03-30 15:53:15 +0530

    039-test orchestration service

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("ZORAB hackathon !");
+	res.send("Orchestration test");
 });
 
 app.listen(3000, "0.0.0.0", () => {
