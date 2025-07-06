be5951c3bace7c5aa023327c73e9a35d32971e89
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2025-07-06 12:48:00 +0530

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello From API");
+  res.send("Hello From Application");
 });
 
 app.listen(3000, "0.0.0.0", () => {
